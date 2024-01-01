03eea97c1873c19ce1de98a2b7f6c07ee871ce16
koala91 <731390309@qq.com>
2024-01-01 23:57:03 +0800

    优化代码

--- src/main/java/com/market/supermarket/LittleSuperMarket.java
+++ src/main/java/com/market/supermarket/LittleSuperMarket.java
@@ -5,22 +5,6 @@ public class LittleSuperMarket {
     public String address;
     public int parkingCount;
     public double incomingSum;
-    public MerchandiseV2 [] merchandises;
+    public Merchandise [] merchandises;
     public int[] merchandiseSold;
-
-//    返回值可以是类名，实际返回值就是这个类的引用
-    public MerchandiseV2 getBiggestProfitMerchandise() {
-        MerchandiseV2 curr = null;
-        for (int i = 0; i < merchandises.length; i++) {
-            MerchandiseV2 m = merchandises[i];
-            if (curr == null) {
-                curr = m;
-                continue;
-            }
-            if (curr.calculateProfit() < m.calculateProfit()) {
-                curr = m;
-            }
-        }
-        return  curr;
-    }
 }
