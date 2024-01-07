70cdb62cdac4f3023ab1933cf324b032c76b3a64
koala91 <731390309@qq.com>
2024-01-08 01:55:29 +0800

    55-构造实例的方法

--- src/main/java/com/market/supermarket/MerchandiseV2DescAppMain.java
+++ src/main/java/com/market/supermarket/MerchandiseV2DescAppMain.java
@@ -2,9 +2,10 @@ package com.market.supermarket;
 
 public class MerchandiseV2DescAppMain {
     public static void main(String[] args) {
-        MerchandiseV2 merchandise = new MerchandiseV2();
-
-        merchandise.init("书桌", "DESK123", 40, 999.9, 500);
+//        MerchandiseV2 merchandise = new MerchandiseV2();
+//
+//        merchandise.init("书桌", "DESK123", 40, 999.9, 500);
+        MerchandiseV2WithConstructor merchandise = new MerchandiseV2WithConstructor("书桌", "DESK123", 40, 999.9, 500);
 
         merchandise.describe();
     }
---
+++ src/main/java/com/market/supermarket/MerchandiseV2WithConstructor.java
@@ -0,0 +1,24 @@
+package com.market.supermarket;
+
+public class MerchandiseV2WithConstructor {
+    public  String name;
+    public  String id;
+    public  int count;
+    public double soldPrice;
+    public double purchasePrice;
+
+//    构造方法的方法名必须和类名一样，而且构造方法没有返回值，这样的方法才是构造方法
+
+    public MerchandiseV2WithConstructor(String name, String id, int count, double soldPrice, double purchasePrice) {
+        this.name = name;
+        this.id = id;
+        this.count = count;
+        this.soldPrice = soldPrice;
+        this.purchasePrice = purchasePrice;
+    }
+    public void describe() {
+        double netIncome = soldPrice - purchasePrice;
+        System.out.println("商品名字叫做" + name + ",id是" + id + "。商品售价是" + soldPrice + "。商品进价是" + purchasePrice + "。商品的库存是" + count + "。商品单个的毛利润是" + netIncome);
+    }
+
+}
